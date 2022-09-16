43512ecffc7663cbafa4e908d77e2b45e99f353a
ManuelTano <manueltano98@gmail.com>
2022-09-16 17:45:34 +0200

    Fix, added style

--- resources/views/products/comic.blade.php
+++ resources/views/products/comic.blade.php
@@ -22,18 +22,53 @@
 
     <section id="white" class="py-5">
         <div class="small-container">
-           <div class="row">
-            <div class="col-8">
-                <div class="cards">
-                    <img src="{{ $comic['thumb'] }}" alt="" class="thumb">
-                    <h4 class="title fw-bold">{{ $comic['title'] }}</h4>
-                    <h6>{{ $comic['series'] }}</h6>
-                    <p> {{ $comic['price'] }}</p>
-                    <p>{{ $comic['description'] }}</p>
+            <div class="row">
+                <div class="col-8 m-0 p-0 ">
+                    <div class="cards">
+                        <img src="{{ $comic['thumb'] }}" alt="" class="thumb">
+                        <h4 class="title fw-bold mb-4">{{ $comic['title'] }}</h4>
+                        <div class="row margin border-green">
+                            <div class="col-9 border-right bg-green d-flex p-2 justify-content-between">
+                                <span class="green fw-bold">U.S. price:
+                                    <span class="text-white">{{ $comic['price'] }}</span></span>
+                                <span class="green fw-bold">AVAILABLE</span>
+                            </div>
+                            <div class="col-3 bg-green p-2 text-center">
+                                <span class="text-white fw-bold">Check Availability</span>
+                            </div>
+                        </div>
+                        <p class="mt-4">{{ $comic['description'] }}</p>
+                    </div>
+                </div>
+                <div class="col-4 text-end">
+                    <h5 class="text-grey">ADVERTISEMENT</h5>
+                    <img src="{{ asset('../images/madv.jpg') }}" alt="adv">
                 </div>
             </div>
-           </div>
         </div>
+    </section>
+
+    <section id="grigio">
+        <div class="small-container">
+
+            <div class="row">
+                <div class="col-6">
+                    <div class="row">
+                        <h3>Talent</h3>
+                    </div>
+                    <div class="row">
+                        <div class="col-3">
+                            Art by:
+                        </div>
+                        <div class="col-9">
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+        </div>
+
     </section>
 
     @include('layout.footer')
